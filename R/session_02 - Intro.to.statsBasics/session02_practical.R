d7616f8f54f67525706b87173589e4f7321f3461
Christopher Maronga <you@example.commarongachristopher@gmail.com>
2021-09-29 00:27:50 +0100

    session 02 materials

---
+++ R/session_02 - Intro.to.statsBasics/session02_practical.R
@@ -0,0 +1,151 @@
+# Installing packages -----------------------------------------------------
+
+
+
+
+
+
+# Loading packages --------------------------------------------------------
+
+
+
+
+
+
+
+# Loading datasets --------------------------------------------------------
+# - birth_weight.csv
+# - babies.csv
+# - birth_weight-fup.csv
+
+
+
+
+
+
+# Transforming data from long to wide and back ----------------------------
+# use modified birth weight follow up
+
+
+
+
+# Variable transformations ------------------------------------------------
+# creating factors
+# Handling dates using lubridate()
+
+
+
+
+
+
+# Functions to have a view of your data -----------------------------------
+# summary(); table()[one-way and 2-way];  head(); tail(); nrow(); ncol()
+# str(); glimpse()
+# skim() and Hmisc::describe()
+
+
+
+
+
+
+
+# Descriptive statistics (1/2) - numericals -------------------------------------
+# mean(), median() and quantile()
+# min(), max() and range()
+# sd(), var() and uncertainty
+
+
+
+
+
+
+# Descriptive statistics (1/2) - expanded, by group -----------------------
+# use of dplyr verbs to manually code
+# Group summary
+# Create simple contigency tables
+
+
+
+
+
+
+
+
+
+# Robust contingency tables using table1() package ------------------------
+# step 1. label() your variables - give them meaningful names
+# step 2. Decide on your grouping variable (top row)
+# step 3 . Apply the function -- checkout ?table1::table1 with[topclass = "Rtable1-zebra"]
+
+# --- For complex contingency tables, check out the qwraps2()
+
+
+# label data
+
+
+
+# Create a simple table (r x 1)
+
+
+
+
+# Create a contingency table (rxc)
+
+
+
+
+# Exploring data via visualizations ---------------------------------------
+# ++ Histogram -----
+
+
+# ++ Density plot -----
+
+
+# ++ Scatter plot -----
+
+
+# ++ Box plot -----
+
+
+# ++ Bar plot -----
+
+
+
+
+# ++ Faceting plots -----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
